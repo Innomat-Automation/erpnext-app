44f59ed637debc5c1d5303fb88338d67f4efbf6d
lasalesi <larsericmueller@gmail.com>
2021-03-18 16:31:40 +0100

    improved calculation

--- innomat/innomat/utils.py
+++ innomat/innomat/utils.py
@@ -8,7 +8,7 @@ from frappe import _
 This function will return the BOM cost/rate for calculations (e.g. quotation)
 """
 @frappe.whitelist()
-def get_bom_rate(item_code)
+def get_bom_rate(item_code):
     boms = frappe.get_all("BOM", 
            filters={'item': item_code, 'is_active': 1, 'is_default': 1}, 
            fields=['name', 'total_cost'])
