f603252dd6437056cebefafa51457b111efc47ee
innomat <info@innomat.ch>
2021-06-26 21:42:51 +0200

    Anpassung am Migrationsscripts

--- innomat/innomat/doctype/bom_template/bom_template.py
+++ innomat/innomat/doctype/bom_template/bom_template.py
@@ -3,8 +3,88 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
-from frappe.model.document import Document
+import frappe
+import erpnext
+from frappe.utils import cint, cstr, flt
+from frappe import _
+from frappe.website.website_generator import WebsiteGenerator
 
-class BOMTemplate(Document):
-	pass
+
+from six import string_types
+
+
+class BOMTemplate(WebsiteGenerator):
+
+    def get_bom_material_detail(self, args=None):
+        """ Get raw material details like uom, desc and rate"""
+        if not args:
+            args = frappe.form_dict.get('args')
+
+        if isinstance(args, string_types):
+            import json
+            args = json.loads(args)
+
+        item = self.get_item_det(args['item_code'])
+
+        args['bom_no'] = args['bom_no'] or item and cstr(
+            item[0]['default_bom']) or ''
+        args['transfer_for_manufacture'] = (cstr(args.get('include_item_in_manufacturing', '')) or
+                                            item and item[0].include_item_in_manufacturing or 0)
+        args.update(item[0])
+
+        rate = self.get_rm_rate(args)
+        ret_item = {
+            'item_name'	: item and args['item_name'] or '',
+            'description': item and args['description'] or '',
+            'image'		: item and args['image'] or '',
+            'stock_uom'	: item and args['stock_uom'] or '',
+            'uom'			: item and args['stock_uom'] or '',
+            'conversion_factor': 1,
+            'bom_no'		: args['bom_no'],
+            'rate'			: rate,
+            'qty'			: args.get("qty") or args.get("stock_qty") or 1,
+            'stock_qty'	: args.get("qty") or args.get("stock_qty") or 1,
+            'base_rate'	: rate,
+            'include_item_in_manufacturing': cint(args['transfer_for_manufacture']) or 0
+        }
+
+        return ret_item
+
+    def get_item_det(self, item_code):
+        item = frappe.db.sql("""select name, item_name, docstatus, description, image,
+            is_sub_contracted_item, stock_uom, default_bom, last_purchase_rate, include_item_in_manufacturing
+            from `tabItem` where name=%s""", item_code, as_dict=1)
+
+        if not item:
+            frappe.throw(
+                _("Item: {0} does not exist in the system").format(item_code))
+
+        return item
+
+    def get_rm_rate(self, arg):
+        return 1.0 #flt(self.get_valuation_rate(arg) / 1)
+
+    def get_valuation_rate(self, args):
+        """ Get weighted average of valuation rate from all warehouses """
+
+        total_qty, total_value, valuation_rate = 0.0, 0.0, 0.0
+        for d in frappe.db.sql("""select actual_qty, stock_value from `tabBin`
+            where item_code=%s""", args['item_code'], as_dict=1):
+                total_qty += flt(d.actual_qty)
+                total_value += flt(d.stock_value)
+
+        if total_qty:
+            valuation_rate =  total_value / total_qty
+
+        if valuation_rate <= 0:
+            last_valuation_rate = frappe.db.sql("""select valuation_rate
+                from `tabStock Ledger Entry`
+                where item_code = %s and valuation_rate > 0
+                order by posting_date desc, posting_time desc, creation desc limit 1""", args['item_code'])
+
+            valuation_rate = flt(last_valuation_rate[0][0]) if last_valuation_rate else 0
+
+        if not valuation_rate:
+            valuation_rate = frappe.db.get_value("Item", args['item_code'], "valuation_rate")
+
+        return valuation_rate
--- innomat/innomat/migration.py
+++ innomat/innomat/migration.py
@@ -4,16 +4,119 @@
 
 
 import frappe
+from frappe.exceptions import TemplateNotFoundError
 import pymssql
 from frappe import _
 
 
-# function to migrate projects from Timesafe
-def migrate_projects_from_timesafe():
-    con = get_connection();
-    cursor = con.cursor(as_dict=True)
-    cursor.execute('SELECT * From Projects')
+# Migrate timesafe to erpnext
+def migrate_to_erpnext(user,password):
+    # get all projects
+    con = get_connection(user,password)
+    projectcursor = con.cursor(as_dict=True)
+    projectcursor.execute('SELECT [strProjectNo] FROM [TimesafeBack].[dbo].[ViewActivities] GROUP BY [strProjectNo]')
 
+    for row in projectcursor:
+        key = row['strProjectNo']
+        company_key = ""
+        if(str(key).startswith('2')):
+            company_key = "ASS"
+        elif(str(key).startswith('3')):
+            company_key = "ASP"
+        elif(str(key).startswith('4')):
+            company_key = "INS"
+        elif(str(key).startswith('5')):
+            company_key = "INP"
+
+        print("Check {0}P{1}".format(company_key, key))
+        doc = frappe.db.exists("Project","{0}P{1}".format(company_key, key))
+        # if project not extist create 
+        if(doc == None):
+            doc = create_project(con,key)
+        
+
+        
+
+
+
+
+def create_project(con,projectname):
+    # get project from timesafe
+    projectcursor = con.cursor(as_dict=True)
+    projectcursor.execute('SELECT * From tProjects WHERE strProjectNo LIKE ' + projectname)
+
+    if projectcursor.rowcount > 1:
+        print("Error more then one project found")
+    else:  
+        for row in projectcursor:
+            # create project 
+            new_project = frappe.get_doc({
+                "doctype": "Project",
+                "project_key": projectname,
+                "project_name": get_erp_projectname(projectname),
+                "project_type": "Project" if str(row['tinState']).startswith('3') or str(row['tinState']).startswith('5') else "Service",
+                "is_active": "Yes",
+                "status": "Open" if row['tinState'] == 1 else "Closed",
+                "title": get_erp_projectname(projectname) + " " + row['strProjectName']
+            })
+            if (row['dtStart'] != None):
+                new_project.expected_start_date = row['dtStart']
+            if (row['dtEnd'] != None):
+                new_project.expected_end_date = row['dtEnd']
+
+            # Add Teammember
+            teammembers = get_team_member(con,row['lProjectID'])
+            if len(teammembers) > 0:
+                new_project.project_team = teammembers
+            
+            print("Insert project " + new_project.title)
+            new_project.insert()
+
+            return new_project
+    
+
+def get_team_member(con,projectid):
+    # get project from timesafe
+    membercursor = con.cursor(as_dict=True)
+    membercursor.execute('SELECT tEmployees.lEmployeeID, tProjectTeam.bProjectLeader, tEmployees.strLogin, tEmployees.strLastName, tEmployees.strFirstName FROM tEmployees LEFT OUTER JOIN tProjectTeam ON tEmployees.lEmployeeID = tProjectTeam.lEmployeeID WHERE tProjectTeam.lProjectID = ' + str(projectid))
+
+    members = []
+    for meb in membercursor:
+        print("Check team member " + str(meb["lEmployeeID"]))
+        userid = frappe.get_all("Employee",filters={"employee_number":meb["lEmployeeID"]})
+        if len(userid) == 1:
+            user = frappe.get_doc("Employee",userid[0])
+            print("Add team member " + user.name)
+            member = frappe.get_doc({
+                "doctype": "Project Member",
+                "employee": user,
+                "project_manager": 0 if meb['bProjectLeader'] == 0 else 1
+            })
+            members.append(member)
+    
+    return members
+
+
+
+def get_erp_projectname(key):
+    company_key = ""
+    if(str(key).startswith('2')):
+        company_key = "ASS"
+    elif(str(key).startswith('3')):
+        company_key = "ASP"
+    elif(str(key).startswith('4')):
+        company_key = "INS"
+    elif(str(key).startswith('5')):
+        company_key = "INP"
+    
+    return "{0}P{1}".format(company_key, key)
+
+
+# function to migrate projects from Timesafe
+def migrate_projects_from_timesafe(user,password):
+    con = get_connection(user,password);
+    cursor = con.cursor(as_dict=True)
+    cursor.execute('SELECT * From Projects WHERE strProjectNr = ')
     for row in cursor:
         key = row['strProjectNo']
         company_key = "IN"
@@ -40,5 +143,5 @@ def migrate_projects_from_timesafe():
     con.close()
     
 
-def get_connection():
-    return pymssql.connect("192.168.80.25\Timesafe","sa","Websilas79","TimesafeBack")
+def get_connection(user,password):
+    return pymssql.connect("192.168.80.25\Timesafe",user,password,"TimesafeBack")
