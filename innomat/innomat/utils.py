35f991e1a99cb8b6d31956c29f716c1c722c385b
lasalesi <larsericmueller@gmail.com>
2021-07-19 21:24:48 +0200

    bugfix: undefined currency in some cases

--- innomat/innomat/utils.py
+++ innomat/innomat/utils.py
@@ -227,13 +227,7 @@ Shortcut to create delivery notes from timesheet
 @frappe.whitelist()
 def create_dn(project, item, qty, description, timesheet):
     pj = frappe.get_doc("Project", project)
-    if pj.sales_order:
-        sales_order = frappe.get_doc("Sales Order", pj.sales_order)
-        currency = sales_order.currency
-    if pj.customer:
-        customer = frappe.get_doc("Customer", pj.customer)
-        if not currency:
-            currency = customer.default_currency
+    currency get_currency(pj)
     new_dn = frappe.get_doc({
         "doctype": "Delivery Note",
         "customer": pj.customer,
@@ -260,13 +254,7 @@ Shortcut to create on call fees from timesheet
 @frappe.whitelist()
 def create_on_call_fee(project, date, timesheet):
     pj = frappe.get_doc("Project", project)
-    if pj.sales_order:
-        sales_order = frappe.get_doc("Sales Order", pj.sales_order)
-        currency = sales_order.currency
-    if pj.customer:
-        customer = frappe.get_doc("Customer", pj.customer)
-        if not currency:
-            currency = customer.default_currency
+    currency get_currency(pj)
     date = datetime.strptime(date, "%Y-%m-%d")
     new_dn = frappe.get_doc({
         "doctype": "Delivery Note",
@@ -313,13 +301,7 @@ def create_travel_notes(timesheet, travel_key):
     dns = []
     for k, v in travel.items():
         pj = frappe.get_doc("Project", k)
-        if pj.sales_order:
-            sales_order = frappe.get_doc("Sales Order", pj.sales_order)
-            currency = sales_order.currency
-        if pj.customer:
-            customer = frappe.get_doc("Customer", pj.customer)
-            if not currency:
-                currency = customer.default_currency
+        currency get_currency(pj)
         new_dn = frappe.get_doc({
             "doctype": "Delivery Note",
             "customer": pj.customer,
@@ -383,13 +365,7 @@ def create_expense_notes(expense_claim, expense_key):
     dns = []
     for k, v in travel.items():
         pj = frappe.get_doc("Project", k)
-        if pj.sales_order:
-            sales_order = frappe.get_doc("Sales Order", pj.sales_order)
-            currency = sales_order.currency
-        if pj.customer:
-            customer = frappe.get_doc("Customer", pj.customer)
-            if not currency:
-                currency = customer.default_currency
+        currency get_currency(pj)
         new_dn = frappe.get_doc({
             "doctype": "Delivery Note",
             "customer": pj.customer,
@@ -463,13 +439,7 @@ def create_sinv_from_project(project, from_date=None, to_date=None, sales_item_g
     delivery_notes = frappe.get_all("Delivery Note", filters={'project': project, 'docstatus': 1, 'status': 'To Bill'}, fields=['name'])
     if len(time_logs) > 0 or len(delivery_notes) > 0:
         pj = frappe.get_doc("Project", project)
-        if pj.sales_order:
-            sales_order = frappe.get_doc("Sales Order", pj.sales_order)
-            currency = sales_order.currency
-        if pj.customer:
-            customer = frappe.get_doc("Customer", pj.customer)
-            if not currency:
-                currency = customer.default_currency
+        currency get_currency(pj)
         new_sinv = frappe.get_doc({
             "doctype": "Sales Invoice",
             "customer": pj.customer,
@@ -1021,3 +991,17 @@ def close_completed_tasks(timesheet, close=1):
             t.save()
     frappe.db.commit()
     return
+
+"""
+Find currency for project
+"""
+def get_currency(project):
+    if pj.sales_order:
+        sales_order = frappe.get_doc("Sales Order", pj.sales_order)
+        currency = sales_order.currency
+    elif pj.customer:
+        customer = frappe.get_doc("Customer", pj.customer)
+        currency = customer.default_currency or "CHF"
+    else:
+        currency = "CHF"
+    return currency
