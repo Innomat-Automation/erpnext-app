9fd152af6bcbfa17168557958f24d5c970cc3a3c
innomat <info@innomat.ch>
2021-08-07 19:52:58 +0200

    Anpassungen am Regex für Feldprüfung Grossbuchstaben haben gefehlt

--- innomat/public/js/purchase_invoice.js
+++ innomat/public/js/purchase_invoice.js
@@ -2,9 +2,9 @@
 
 frappe.ui.form.on("Purchase Invoice", {
     "bill_no": function(frm) {
-        if(!frm.doc.bill_no.match("^[a-z0-9\+\?\/\-\:\(\)\.\,\']+$"))
+        if(!frm.doc.bill_no.match("^[ A-Za-z0-9\+\?\/\-\:\(\)\.\,\']+$"))
             {
-                frappe.msgprint("Nur alphanumerische Zeichen und +?/-;/()., erlaubt");
+                frappe.msgprint("Nur alphanumerische Zeichen und +?/-:/()., erlaubt");
             }
 
     }
