3b0b05340ac870729d60dbbf0b96d4e78bd4f4b2
innomat <info@innomat.ch>
2021-08-29 19:17:00 +0200

    Korrektur bei Pattern, aus irgendeinem Grund braucht es zwei Backslash vor dem Bindestrich

--- innomat/public/js/purchase_invoice.js
+++ innomat/public/js/purchase_invoice.js
@@ -2,7 +2,7 @@
 
 frappe.ui.form.on("Purchase Invoice", {
     "bill_no": function(frm) {
-        if(!frm.doc.bill_no.match("^[ A-Za-z0-9\+\?\/\-\:\(\)\.\,\']+$"))
+        if(!frm.doc.bill_no.match("^[ A-Za-z0-9\+\?\/\\-\:\(\)\.\,\']+$"))
             {
                 frappe.msgprint("Nur alphanumerische Zeichen und +?/-:/()., erlaubt");
             }
