c638209dbbdea5391ab77d11d5896aa4c99e38c8
innomat <info@innomat.ch>
2022-06-08 20:35:52 +0200

    Anpassungen Remote Access

--- innomat/innomat/doctype/remote_access/remote_access.js
+++ innomat/innomat/doctype/remote_access/remote_access.js
@@ -6,3 +6,22 @@ frappe.ui.form.on('Remote Access', {
 
 	// }
 });
+
+
+frappe.ui.form.on('Remote Access Point', {
+    copy_password(frm, cdt, cdn) {
+        frappe.call({
+            "method": "innomat.innomat.scripts.remote_access.decrypt_access_password",
+            "args": {
+                "cdn": cdn
+            },
+            "callback": function(response) {
+                navigator.clipboard.writeText(response.message).then(function() {
+                    frappe.show_alert( __("Copied") );
+                  }, function() {
+                     frappe.show_alert( __("No access") );
+                });
+            }
+        });
+    }
+});
