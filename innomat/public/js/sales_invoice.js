4c153c12acf99d0324bc8e034969e3d0a26c6c67
innomat <info@innomat.ch>
2022-06-08 20:54:19 +0200

    Delete Contact Person

--- innomat/public/js/sales_invoice.js
+++ innomat/public/js/sales_invoice.js
@@ -7,5 +7,9 @@ frappe.ui.form.on('Sales Invoice', {
     },
     company(frm) {
         fetch_tax_rule(frm);
+    },
+    delete_contact(frm){
+        frm.set_value("contact_display",'');
+        frm.set_value("contact_person",null)
     }
 });
