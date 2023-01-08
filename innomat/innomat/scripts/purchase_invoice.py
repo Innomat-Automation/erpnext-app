0e23989909b6a1e888356bc37beb2f8631e63f0c
Marco Müller <marco@ms-mueller.ch>
2023-01-08 22:01:49 +0100

    Zuordnen von Dokumenten zu Projekten nach Buchen (#40)
    Co-authored-by: innomat <info@innomat.ch>

---
+++ innomat/innomat/scripts/purchase_invoice.py
@@ -0,0 +1,51 @@
+
+import frappe
+import json
+from frappe import _
+
+
+
+@frappe.whitelist()
+def update_projects(data,invoice):
+    jsondata = json.loads(data)
+    
+    project_list = []
+    
+    if "project" in jsondata.keys() and jsondata['project'] and jsondata['project'] != None:
+        project = frappe.get_doc("Project", jsondata['project'])
+        if project:
+            project_list.append(jsondata['project'])
+            frappe.db.sql("""UPDATE `tabPurchase Invoice Item` SET `project` = '{proj}' WHERE `parent` = '{name}' """.format(name=invoice, proj=jsondata['project']))
+    
+    for key,value in jsondata.items():
+        if key == "project":
+            continue
+        if not value in project_list:
+            project = frappe.get_doc("Project", value)
+            if project:
+                project_list.append(value)
+            else:
+                continue
+       
+        frappe.db.sql("""UPDATE `tabPurchase Invoice Item` SET `project` = '{proj}' WHERE `name` = '{name}' """.format(name=key, proj=value))
+    
+    for pro in project_list:
+        project = frappe.get_doc("Project", pro)
+        project.update_purchase_costing()
+        project.db_update()
+    
+    return "Done"
+
+@frappe.whitelist()
+def delete_projects(invoice):
+    project_list = frappe.db.sql("""SELECT project FROM `tabPurchase Invoice Item` WHERE `parent` = '{name}' AND NOT ISNULL(project)""".format(name=invoice),as_dict=1)
+    
+    frappe.db.sql("""UPDATE `tabPurchase Invoice Item` SET `project` = Null WHERE `parent` = '{name}' """.format(name=invoice))
+    
+    for pro in project_list:
+        project = frappe.get_doc("Project", pro['project'])
+        project.update_purchase_costing()
+        project.db_update()
+        
+    return "Done"
+    
--- innomat/public/js/purchase_order.js
+++ innomat/public/js/purchase_order.js
@@ -14,6 +14,53 @@ frappe.ui.form.on('Purchase Order', {
         frm.add_custom_button(__("Bulk Export"), function() {
             bulk_export(frm);
         });
+        if (frm.doc.docstatus === 1) {
+            frm.add_custom_button(__("Update Project Link"), function() {
+                var field = new Array();
+                
+                field.push({label : __("Project"), fieldname : "project", fieldtype: 'Link', "options": "Project"})
+                frm.doc.items.forEach(e => field.push({label : e.idx + ':' + e.item_code, fieldname : e.name, fieldtype: 'Link', "options": "Project", default: e.project}))
+
+                let d = new frappe.ui.Dialog({
+                    title: 'Items',
+                    fields: field,
+                    primary_action_label: 'Submit',
+                    primary_action(values) {
+                        d.hide();
+                        frappe.call({
+                            "method": "innomat.innomat.scripts.purchase_order.update_projects",
+                            "args": {
+                                "data": values,
+                                "invoice" : frm.doc.name
+                            },
+                            "callback": function(response) {
+                                var data = response.message;
+                                console.log(data);
+                                frm.reload_doc();
+                            }
+                        });
+                    }
+                });
+                d.show();
+            });
+            frm.add_custom_button(__("Delete all Project Link"), function() {
+                frappe.confirm(__("Delete all Project links"),
+                () => {
+                    frappe.call({
+                        "method": "innomat.innomat.scripts.purchase_order.delete_projects",
+                        "args": {
+                            "invoice" : frm.doc.name
+                        },
+                        "callback": function(response) {
+                            var data = response.message;
+                            console.log(data);
+                            frm.reload_doc();
+                        }
+                    });
+                },
+                () => {});
+            });
+        }
     },
     before_save(frm) {
        // assure each item has a project assigned
