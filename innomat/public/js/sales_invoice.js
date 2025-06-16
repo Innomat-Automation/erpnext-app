aaae9e1e82e4abf93cfc1943b300feb9dfb1f77a
lasalesi <larsericmueller@gmail.com>
2025-06-16 16:06:27 +0200

    Apply cost center to item positions on sales invoice

--- innomat/public/js/sales_invoice.js
+++ innomat/public/js/sales_invoice.js
@@ -59,6 +59,8 @@ frappe.ui.form.on('Sales Invoice', {
     },
     before_save(frm) {
         apply_discount_from_akonto(frm);
+
+	apply_cost_center(frm);
     },
     additional_discount_percentage_akonto(frm){
         frm.doc.additional_discount_amount_akonto = flt(frm.doc.total * flt(frm.doc.additional_discount_percentage_akonto) / 100, precision("additional_discount_amount_akonto"));
@@ -155,3 +157,10 @@ function apply_discount_from_akonto(frm) {
     cur_frm.set_value("discount_amount", akonto_discount);
 }
 
+function apply_cost_center(frm) {
+    if (frm.doc.items) {
+        for (let i = 0; i < frm.doc.items.length; i++) {
+	    frappe.model.set_value(frm.doc.items[i].doctype, frm.doc.items[i].name, 'cost_center', frm.doc.cost_center);
+	}
+    }
+}
